4e5481531ffd0ab2b64d9c1bac4aafd9a7464742
Georgiy <lizgin.gv@phystech.edu>
2022-02-12 23:21:43 +0300

    commit

--- backend/foodgram_backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/foodgram_backend/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '62.84.123.12',
+    ]
 
 # Application definiton
 
