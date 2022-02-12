9a4fead20feaf80b9f0efedcbc0d6e96a6aae71a
Georgiy <lizgin.gv@phystech.edu>
2022-02-13 02:20:57 +0300

    commit

--- backend/foodgram_backend/food/views.py
+++ backend/foodgram_backend/food/views.py
@@ -39,6 +39,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         tags_slugs = self.request.query_params.get('tags')
+        return HttpResponse(tags_slugs, status=status.HTTP_204_NO_CONTENT)
         print(tags_slugs)
         recipes = Recipe.objects.all()
         if tags_slugs is not None:
