2d959391bc698e747f7606a3322fd24e0fd44423
Georgiy <lizgin.gv@phystech.edu>
2022-02-14 11:22:01 +0300

    commit

--- backend/foodgram_backend/users/serializers.py
+++ backend/foodgram_backend/users/serializers.py
@@ -45,17 +45,17 @@ class SubscriptionSerializer(UserSerializer):
         ) + ('is_subscribed', 'recipes')
 
     def get_recipes(self, obj):
-        recipe_limit = self.context.get('request').query_params.get('recipe_limit')
+        recipe_limit = self.context['request'].query_params.get('recipe_limit')
         if recipe_limit:
             recipe_limit = int(recipe_limit)
-            data = obj.recipes.all()[recipe_limit]
+            data = obj.recipes.all()[:recipe_limit]
             serialized_data = RecipePartSerializer(
                 data=data,
                 many=True)
             serialized_data.is_valid()
             return serialized_data.data
         serialized_data = RecipePartSerializer(
-            data=obj.recipes.all(),
+            data=obj.recipes.all()[:3],
             many=True)
         serialized_data.is_valid()
         return serialized_data.data
